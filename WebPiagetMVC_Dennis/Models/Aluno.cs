304d9827e3fd8be0ab413a29a02305b76cada76c
“Dsantos33” <dennis.santos3312@gmail.com>
2025-03-25 11:04:00 -0300

    Adicionar arquivos de projeto.

---
+++ WebPiagetMVC_Dennis/Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace WebPiagetMVC_Dennis.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public String? Nome { get; set; }
+        public String? Professor { get; set; }
+        public DateTime Idade { get; set; }
+    }
+}
